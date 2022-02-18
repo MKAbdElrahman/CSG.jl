da656fc2fc537ff4105b40ca48656fd72d614cfa
Documenter.jl <documenter@juliadocs.github.io>
2022-02-18 20:49:52 +0000

    build based on 066e2ba

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CSG","category":"page"},{"location":"#CSG","page":"Home","title":"CSG","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CSG.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CSG]","category":"page"}]
+}
